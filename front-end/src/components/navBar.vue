c92f50e367d01f62cbced9ac9607819054751c7c
SergioHMagalhaes <ssergioh3@gmail.com>
2022-11-14 17:06:30 -0300

    feat: Modifica navbar quando usuário estiver autenticado

--- front-end/src/components/navBar.vue
+++ front-end/src/components/navBar.vue
@@ -6,7 +6,7 @@
                     <div class="divLogo">
                         <a class="navbar-brand" href="/"><img src="../assets/logo.svg" class="logo" alt="Logo"></a>
                     </div>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button v-if="!logged" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
                     
@@ -22,14 +22,28 @@
                                 <a class="nav-link" href="/premium">Premium</a>
                             </li>
                         </ul>
-                        <div class="buttonInsideMenu">
+                        <div class="buttonInsideMenu" v-if="!logged">
                             <a href="/signup" class="buttonRegister">Cadastrar</a>
                             <a href="/login" class="buttonLogin">Entrar</a>
                         </div>
                     </div>
-                    <div class="buttonOutsideMenu">
+                    <div class="buttonOutsideMenu" v-if="!logged">
                             <button class="buttonRegister"><a href="/signup">Cadastrar</a></button>
                             <button class="buttonLogin"><a href="/login">Entrar</a></button>
+                    </div>
+                    <div class="logged dropdown" v-if="logged">
+                        <img src="https://ovicio.com.br/wp-content/uploads/2021/07/20210712-one-piece-zoro-wano-postcover-555x555.jpg"
+                            class="rounded-circle avatar" alt="Avatar" type="button" id="dropdownMenu"
+                            data-bs-toggle="dropdown" aria-expanded="false" />
+                        <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="dropdownMenu">
+                            <li class="dropdown-item nav-dropdown"><a class="nav-link active"  href="/">Inicio</a></li>
+                            <li class="dropdown-item nav-dropdown"><a class="nav-link" href="/trilhas">Trilhas</a></li>
+                            <li class="dropdown-item nav-dropdown"><a class="nav-link" href="/premium">Premium</a></li>
+                            <li><a class="dropdown-item" href="#">Minha lista</a></li>
+                            <li><a class="dropdown-item" href="#">Configurações de perfil</a></li>
+                            <li v-if="user.userGroup == 'Super'"><a class="dropdown-item" href="#">Cadastrar trilha</a></li>
+                            <li class="dropdown-item logOut" @click="logOut">Sair</li>
+                        </ul>
                     </div>
                     
                 </div>
@@ -39,8 +53,38 @@
 </template>
 
 <script>
+import { isSignedIn, signOut } from '../services/auth'
 export default {
-
+    data() {
+        return {
+            logged: false,
+            user: {}
+        }
+    },
+    created(){
+        this.isLoggedIn()
+    },
+    watch: {
+      '$route' () {
+        this.isLoggedIn()
+      }
+    },
+    methods: {
+        isLoggedIn(){
+            const { isLoggedIn, user, /*decoded*/ } = isSignedIn()
+            if(isLoggedIn){
+                const userObject = JSON.parse(user)
+                this.user = userObject
+                this.logged = true
+            } else{
+                this.logged = false
+            }
+        },
+        logOut(){
+            signOut()
+            this.$router.go(this.$router.currentRoute)
+        }
+    }
 }
 </script>
 
@@ -63,7 +107,7 @@ a:hover{
 .navbar{
     color: #FFF;
 }
-.logo{
+.logo, .avatar{
     width: 40px;
 }
 .divLogo{
@@ -99,6 +143,19 @@ a:hover{
 .buttonOutsideMenu{
     display: flex;
 }
+.dropdown-menu{
+    background: rgb(55, 55, 55);
+}
+.dropdown-item:hover{
+    background: rgb(55, 55, 55);
+}
+.logOut{
+    cursor:pointer;
+    color: #FFF;
+}
+.nav-dropdown{
+    display: none;
+}
 
 @media screen and (max-width: 770px){
     .navbarNav{
@@ -110,5 +167,12 @@ a:hover{
     .buttonOutsideMenu{
         display: none;
     }
+
+}
+@media screen and (max-width: 576px){
+    .nav-dropdown{
+    display: block;
+    }
+
 }
 </style>
