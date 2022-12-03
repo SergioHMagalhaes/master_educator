0ffbcfe886b9d43fe42f55bc680dd837d51993a1
SergioHMagalhaes <ssergioh3@gmail.com>
2022-12-03 12:49:45 -0300

    fix: Altera o redirecionamento após login

--- front-end/src/components/genericLoginForm.vue
+++ front-end/src/components/genericLoginForm.vue
@@ -55,7 +55,7 @@ export default {
         async signIn(){
             await signIn(this.modal.email, this.modal.password)
             this.$router.push({
-                path: '/',
+                path: '/trilhas',
             })
         },
         async signUp(){
